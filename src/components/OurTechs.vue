53f01fbc9a0a16e2702578f1a7f800ae7d2df35a
Lucas de Brito <devlucasmail@gmail.com>
2022-07-22 01:14:25 -0300

    Techs # feat: Create the section of technologies, use a slick carousel

--- src/components/OurTechs.vue
+++ src/components/OurTechs.vue
@@ -1,23 +1,72 @@
 <template>
-    <div class="wrapper">
-        <b-container>
-            <b-row>
-                <b-col>
-                    <h1>footer</h1>
-                </b-col>
-            </b-row>
-        </b-container>
-    </div>
+  <div class="wrapper">
+    <b-container>
+      <b-row>
+        <b-col>
+          <h1 class="titles">{{ TEXTS.title[lang] }}</h1>
+        </b-col>
+      </b-row>
+
+      <b-row class="mt-3 mt-lg-5">
+        <b-col class="text-center p-2">
+          <VueSlickCarousel v-bind="carouselSettings">
+            <div
+              v-for="(tech, key) in TEXTS.techs"
+              :key="key"
+              class="
+                d-flex
+                flex-column
+                align-items-center
+                justify-content-center
+              "
+            >
+              <img
+                :src="require(`../assets/ourTechs/${tech}.png`)"
+                :alt="`${tech} logo`"
+                width="48"
+                class="imgs-tech"
+              />
+              <span class="d-block texts"> {{ tech }} </span>
+            </div>
+          </VueSlickCarousel>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import TEXTS from "../static/texts/tech.json";
+import StoreMixin from "./StoreMixin";
+import VueSlickCarousel from "vue-slick-carousel";
 export default {
-
-}
+  name: "OurTechs",
+  mixins: [StoreMixin],
+  components: {
+    VueSlickCarousel,
+  },
+  data: () => ({
+    TEXTS: TEXTS,
+    carouselSettings: {
+      arrows: true,
+      dots: false,
+      infinite: true,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 1000,
+      autoplaySpeed: 3000,
+      cssEase: "ease-in-out",
+      pauseOnDotsHover: false,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+    },
+  }),
+};
 </script>
 
 <style scoped>
 .wrapper {
-    min-height: 40vh;
+  min-height: 40vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import { BootstrapVue, IconsPlugin, BIconInstagram, BIconLinkedin, BIconWhatsapp
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// vue-slick-carousel styles
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
 Vue.config.productionTip = false
 Vue.prototype.$store = store;
 Vue.use(BootstrapVue, IconsPlugin)
